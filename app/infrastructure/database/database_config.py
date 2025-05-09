9119adf8df107a50de3cbb6e1ca7829e297e8feb
Rodrigo Leão <roliveira@weg.net>
2025-05-09 11:44:36 -0300

    atualizando

--- app/infrastructure/database/database_config.py
+++ app/infrastructure/database/database_config.py
@@ -1,8 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import settings
+from typing import AsyncGenerator
 
-DATABASE_URL = settings.DATABASE_URL
+DATABASE_URL = settings.DATABASE_URI
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(
@@ -11,7 +12,7 @@ AsyncSessionLocal = sessionmaker(
 
 Base = declarative_base()
 
-async def get_db() -> AsyncSession:
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionLocal() as session:
         yield session
 
