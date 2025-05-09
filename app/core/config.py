f0d969b2f928950dcd9eb84e8fa860bd85cdc148
Rodrigo de Oliveira <roliveira@weg.net>
2025-05-09 10:44:27 -0300

    atualizando arqs

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,10 @@
 from pydantic_settings import BaseSettings
 from typing import Optional, List
+from urllib.parse import quote_plus
+
+# Formato correto para a string de conexão
+password = quote_plus("b@p5rk8&9BJRVEQ")
+db_uri = f"postgresql://5e0dceda-d930-5742-a8d9-1f2d1ff22159:{password}@qas-postgresql-ap.weg.net:40030/automacaopmopostgre"
 
 class Settings(BaseSettings):
     # Informações gerais da API
@@ -7,10 +12,9 @@ class Settings(BaseSettings):
     API_VERSION: str = "1.0.0"
     root_path: Optional[str] = None
     swagger_servers_list: Optional[str] = None
-<<<<<<< HEAD
     
     # Configurações do banco de dados PostgreSQL da WEG
-    DATABASE_URI: str = "postgresql://5e0dceda-d930-5742-a8d9-1f2d1ff22159:b@p5rk8&9BJRVEQ@qas-postgresql-ap.weg.net:40030/automacaopmopostgre"
+    DATABASE_URI: str = db_uri
     
     # Segurança
     SECRET_KEY: str = "supersecretkey"
@@ -25,22 +29,6 @@ class Settings(BaseSettings):
     JIRA_USERNAME: str = "your-jira-email@example.com"
     JIRA_API_TOKEN: str = "your-jira-api-token"
     
-=======
-
-    # PostgreSQL Database Configuration
-    POSTGRES_USER: str = "user"
-    POSTGRES_PASSWORD: str = "password"
-    POSTGRES_SERVER: str = "localhost"
-    POSTGRES_PORT: str = "5432"
-    POSTGRES_DB: str = "appdb"
-    DATABASE_URL: Optional[str] = None
-
-    def __init__(self, **values):
-        super().__init__(**values)
-        if not self.DATABASE_URL:
-            self.DATABASE_URL = f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
-
->>>>>>> 00dfbfaffe36b844d48fa90831148ec7ad9b9d9d
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
