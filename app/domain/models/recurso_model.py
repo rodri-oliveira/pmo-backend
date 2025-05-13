45246fd78fe62d333947e203fb5e542d4f186be2
Rodrigo Leão <roliveira@weg.net>
2025-05-13 15:05:53 -0300

    recurso swagger ok

--- app/domain/models/recurso_model.py
+++ app/domain/models/recurso_model.py
@@ -1,17 +1,19 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime, date
 
 class Recurso(BaseModel):
     id: int
     nome: str
-    email: EmailStr
-    matricula: Optional[str] = None
-    cargo: Optional[str] = None
-    jira_user_id: Optional[str] = None
-    data_admissao: Optional[date] = None
-    equipe_principal_id: Optional[int] = None
+    email: str
+    matricula: Optional[str]
+    cargo: Optional[str]
+    jira_user_id: Optional[str]
+    data_admissao: Optional[date]
+    equipe_principal_id: Optional[int]
     ativo: bool
     data_criacao: datetime
     data_atualizacao: datetime
 
+    class Config:
+        from_attributes = True
