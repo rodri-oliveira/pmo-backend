2a9e9ceb1da058a3435118511e6754fc2cb4cb0b
Rodrigo Leão <roliveira@weg.net>
2025-06-10 10:41:40 -0300

    autalizando

--- app/repositories/secao_repository.py
+++ app/repositories/secao_repository.py
@@ -1,5 +1,7 @@
 from app.db.orm_models import Secao
 from app.repositories.base_repository import BaseRepository
+from typing import Optional
+from sqlalchemy import select
 
 class SecaoRepository(BaseRepository):
     def __init__(self, db):
@@ -10,3 +12,11 @@ class SecaoRepository(BaseRepository):
 
     def list(self):
         return self.db.query(Secao).all()
+    
+    async def get_by_jira_project_key(self, jira_project_key: str) -> Optional[Secao]:
+        """
+        Busca uma secao pelo jira_project_key.
+        """
+        query = select(self.model).where(self.model.jira_project_key == jira_project_key)
+        result = await self.db.execute(query)
+        return result.scalars().first()
